8f24a1482043d4268989b5c71bce40cf0ef46a48
njmarko <njmarko1991@gmail.com>
2020-05-11 23:20:58 +0200

    Initial project commit

---
+++ TBBMatrixMull/TBBMatrixMull/main.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Marko Njegomir sw-38-2018
+Project: Parallel Matrix Multiplication using Intel's TBB library
+Date: 5/11/2020
+*/
+
+#include <iostream>
+
+int main(int argc, int* argv[]) {
+
+	std::cout << "Hello World!\n";
+
+	return 0;
+}
