e4c4358046c3fbd9883adafb521ebd6556d99795
njmarko <njmarko1991@gmail.com>
2020-05-17 21:02:20 +0200

    Added a new project for parallel version of the algorithm

--- TBBMatrixMull/TBBMatrixMullParallel/main.cpp
+++ TBBMatrixMull/TBBMatrixMullParallel/main.cpp
@@ -1,14 +1,14 @@
 /*
 Author: Marko Njegomir sw-38-2018
 Project: Parallel Matrix Multiplication using Intel's TBB library
-Date: 5/11/2020
+Date: 5/17/2020
 */
 
 #include <iostream>
 
 int main(int argc, int* argv[]) {
 
-	std::cout << "Hello World serial!\n";
+	std::cout << "Hello World parallel!\n";
 
 	return 0;
 }
